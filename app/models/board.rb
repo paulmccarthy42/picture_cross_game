6f7af6e11584baae2d749e36ce1c65a423357af9
Paul McCarthy <phantasos42@gmail.com>
2018-05-05 14:23:59 -0500

    generate map

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,26 @@
 class Board < ApplicationRecord
   has_many :cells
+
+  def map
+    empty_map = []
+    size.times do
+      empty_map << []
+      size.times do
+        empty_map.last << []
+      end
+    end
+
+    cells.each.with_object(empty_map) do |cell, map|
+      map[cell.x_position][cell.y_position] = cell.filled
+    end
+  end
+
+  def as_json
+    {
+      id: id,
+      size: "#{size} by #{size}",
+      completed: completed,
+      cells: map
+    }
+  end
 end
--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,3 +1,11 @@
 class Cell < ApplicationRecord
   belongs_to :board
+
+  def as_json
+    {
+      x_position: x_position,
+      y_position: y_position,
+      filled: filled
+    }
+  end
 end
